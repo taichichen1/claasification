41ee9c55b7b87c1904df6cb38330bf51578db828
taichichen1 <103369792+taichichen1@users.noreply.github.com>
2022-05-08 18:59:19 -0400

    Update streamlitTest2.py

--- streamlitTest2.py
+++ streamlitTest2.py
@@ -30,7 +30,7 @@ classifier_name= st.sidebar.selectbox("Select Classifier", ("KNN", "SVM", "Rando
 def get_dataset(dataset_name):
     if dataset_name == "Iris":
         data = datasets.load_iris()
-    elif dataset_name == "Breast Cancer":
+    elif dataset_name == " Breast Cancer":
         data = datasets.load_breat_cancer()
     else:
         data = datasets.load_wine()
@@ -44,12 +44,12 @@ X, y = get_dataset(dataset_name)
 st.write("shape of dataset", X.shape)
 st.write("number of clesses", len(np.unique(y)))
 
-def add_parameter_ui(cls_name):
+def add_parameter_ui(clf_name):
     params = dict()
-    if cls_name == "KNN":
+    if clf_name == "KNN":
         K = st.sidebar.slider("K",1,15)
         params["K"] = K
-    elif cls_name == "SVM":
+    elif clf_name == "SVM":
         C = st.sidebar.slider("C",0.01,10.0)
         params["C"] = C
     else:
@@ -59,27 +59,27 @@ def add_parameter_ui(cls_name):
         params["n_estimators"]=n_estimators
     return params
 
-def get_classsifier(cls_name,params):
-   
-    if cls_name == "KNN":
-        cls = KNeighborsClassifier(n_neighbors=params["K"])
+def get_classsifier(clf_name,params):
+    if clf_name == "KNN":
+        clf = KNeighborsClassifier(n_neighbors=params["K"])
         
-    elif cls_name == "SVM":
-        cls=SVC(C=params["C"])
+    elif clf_name == "SVM":
+        clf=SVC(C=params["C"])
         
     else:
         clf=RandomForestClassifier(n_estimators=params["n_estimators"],max_depth=params["max_depth"],random_state=1234)
-    return params
+    return clf
 
 params=add_parameter_ui(classifier_name)
 
 clf= get_classsifier(classifier_name, params)
+
 #Classification
 X_train, X_test, y_train, y_test= train_test_split(X,y,test_size=0.2,random_state=1234)
 clf.fit(X_train,y_train)
 
 y_pred=clf.predict(X_test)
-acc = accuracy_score(y_test,y_pred)
+acc =accuracy_score(y_test,y_pred)
 st.write(f"Classifier={classifier_name}")
 st.write(f"accuracy={acc}")
 
