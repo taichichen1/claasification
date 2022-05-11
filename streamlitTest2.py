6036397bddbfbe0a1893c5af8982f2ed330a4b82
taichichen1 <103369792+taichichen1@users.noreply.github.com>
2022-05-11 08:57:03 -0400

    Update streamlitTest2.py

--- streamlitTest2.py
+++ streamlitTest2.py
@@ -30,8 +30,8 @@ classifier_name= st.sidebar.selectbox("Select Classifier", ("KNN", "SVM", "Rando
 def get_dataset(dataset_name):
     if dataset_name == "Iris":
         data = datasets.load_iris()
-    elif dataset_name == " Breast Cancer":
-        data = datasets.load_breat_cancer()
+    elif dataset_name == "Breast Cancer":
+        data = datasets.load_breast_cancer()
     else:
         data = datasets.load_wine()
         
